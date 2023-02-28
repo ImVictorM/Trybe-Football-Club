613ab98cfd958ddea4f42abcd3bf3f48845ed220
Victor Mendes <victorutdsim7@gmail.com>
2023-02-28 12:10:30 -0300

    feat: exclui model de exemplo e descomenta associação em TeamModel

--- app/backend/src/database/models/TeamModel.ts
+++ app/backend/src/database/models/TeamModel.ts
@@ -1,5 +1,5 @@
 import { Model, DataTypes } from 'sequelize';
-// import MatchModel from './MatchModel';
+import MatchModel from './MatchModel';
 import db from '.';
 
 class TeamModel extends Model {
@@ -23,24 +23,25 @@ TeamModel.init({
   sequelize: db,
 });
 
-// TeamModel.hasMany(MatchModel, {
-//   foreignKey: 'home_team_id',
-//   as: 'homeTeam',
-// });
+// create associations between team and matches
+TeamModel.hasMany(MatchModel, {
+  foreignKey: 'home_team_id',
+  as: 'homeTeam',
+});
 
-// TeamModel.hasMany(MatchModel, {
-//   foreignKey: 'away_team_id',
-//   as: 'awayTeam',
-// });
+TeamModel.hasMany(MatchModel, {
+  foreignKey: 'away_team_id',
+  as: 'awayTeam',
+});
 
-// MatchModel.belongsTo(TeamModel, {
-//   foreignKey: 'home_team_id',
-//   as: 'homeTeamId',
-// });
+MatchModel.belongsTo(TeamModel, {
+  foreignKey: 'home_team_id',
+  as: 'homeTeamId',
+});
 
-// MatchModel.belongsTo(TeamModel, {
-//   foreignKey: 'away_team_id',
-//   as: 'awayTeamId',
-// });
+MatchModel.belongsTo(TeamModel, {
+  foreignKey: 'away_team_id',
+  as: 'awayTeamId',
+});
 
 export default TeamModel;
