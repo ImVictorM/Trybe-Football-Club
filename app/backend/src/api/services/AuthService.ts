9e1fc626560a33899c5e988383a09d3ff55576eb
Victor Mendes <victorutdsim7@gmail.com>
2023-02-28 21:34:37 -0300

    feat: serviço que trabalha com autenticações

---
+++ app/backend/src/api/services/AuthService.ts
@@ -0,0 +1,23 @@
+import bcrypt = require('bcryptjs');
+import jwt = require('jsonwebtoken');
+import { IUser } from './interfaces/IServiceUser';
+
+const { JWT_SECRET } = process.env;
+
+class AuthService {
+  public static checkPassword(password: string, hash: string): void {
+    const passwordIsValid = bcrypt.compareSync(password, hash);
+    if (!passwordIsValid) {
+      const error = new Error('All fields must be filled');
+      error.stack = '400';
+      throw error;
+    }
+  }
+
+  public static userToToken(user: IUser) {
+    const token = jwt.sign({ ...user }, JWT_SECRET as jwt.Secret);
+    return token;
+  }
+}
+
+export default AuthService;
