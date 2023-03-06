20dac4051bcd64d8aabe8e240b86937744f28cd7
Victor Mendes <victorutdsim7@gmail.com>
2023-03-06 10:27:33 -0300

    refactor: separa a logica

--- app/backend/src/api/middlewares/ErrorHandler.ts
+++ app/backend/src/api/middlewares/ErrorHandler.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 
 class ErrorHandler extends Error {
   public static handleError(error: Error, _req: Request, res: Response, _next: NextFunction) {
-    const errorStackIsNotANumber = Number.isNaN(Number(error.stack));
-    const errorCode = errorStackIsNotANumber ? 500 : error.stack;
-    return res.status(Number(errorCode)).json({ message: error.message });
+    const errorStack = Number(error.stack);
+    const errorStackIsNotANumber = Number.isNaN(errorStack);
+    const errorCode = errorStackIsNotANumber ? 500 : errorStack;
+    return res.status(errorCode).json({ message: error.message });
   }
 }
 
